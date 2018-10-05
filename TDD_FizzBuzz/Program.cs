877c5726fb7c8105b5f7257dac1c402cc103ef89
Gabriel Souza de Paula <gspaula@netuno.lan>
2018-10-05 10:45:11 -0300

    commit inicial

---
+++ TDD_FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using FizzBuzz.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto TDD Fizz Buzz");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzzer.GetValue(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
